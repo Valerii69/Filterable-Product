8ff085c6b455e89297deecb12556d4e5d1f0b319
Valerii Viun <2455521@gmail.com>
2023-11-22 17:29:29 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import  PRODUCTS  from "./db/products.json";
 
 function FilterableProductTable({ products }) {
   const [filterText, setFilterText] = useState('');
@@ -109,15 +110,6 @@ function SearchBar({
     </form>
   );
 }
-
-const PRODUCTS = [
-  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
-];
 
 export default function App() {
   return <FilterableProductTable products={PRODUCTS} />;
